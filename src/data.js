732590e4f844bf4d12386869e6f2a43e7c9d9ce2
Oleg <126164012+olreyplay@users.noreply.github.com>
2023-06-21 12:50:59 +0300

    add data

---
+++ src/data.js
@@ -0,0 +1,124 @@
+const owners = [
+  {
+    name: "Ramiro Kreiger",
+    vehicle: "Dodge F-150",
+    id: "15",
+  },
+  {
+    name: "Eula Hermiston",
+    vehicle: "Volkswagen Spyder",
+    id: "16",
+  },
+  {
+    name: "Clayton Pfannerstill",
+    vehicle: "Volvo Challenger",
+    id: "18",
+  },
+  {
+    name: "Doyle Runolfsdottir Jr.",
+    vehicle: "Rolls Royce Model S",
+    id: "19",
+  },
+  {
+    name: "Patsy Simonis",
+    vehicle: "Volvo Explorer",
+    id: "20",
+  },
+  {
+    name: "Luz Hirthe",
+    vehicle: "Aston Martin Camry",
+    id: "22",
+  },
+  {
+    name: "Gertrude Swaniawski",
+    vehicle: "Jeep Model Y",
+    id: "23",
+  },
+  {
+    name: "Martin Schaefer",
+    vehicle: "BMW Accord",
+    id: "24",
+  },
+  {
+    name: "Maryann Nader",
+    vehicle: "Mazda Spyder",
+    id: "25",
+  },
+  {
+    name: "Kendra Bauch",
+    vehicle: "Mazda Model 3",
+    id: "26",
+  },
+  {
+    name: "Beverly Schinner",
+    vehicle: "Bugatti LeBaron",
+    id: "27",
+  },
+  {
+    name: "Glen Pollich",
+    vehicle: "Kia Land Cruiser",
+    id: "28",
+  },
+  {
+    name: "Mr. Curtis Vandervort MD",
+    vehicle: "Aston Martin Colorado",
+    id: "29",
+  },
+  {
+    name: "Shelia Berge DDS",
+    vehicle: "Nissan Land Cruiser",
+    id: "30",
+  },
+  {
+    name: "Grady Rodriguez",
+    vehicle: "Volkswagen Mustang",
+    id: "31",
+  },
+  {
+    name: "Mrs. Blanche Gusikowski",
+    vehicle: "Jeep Expedition",
+    id: "32",
+  },
+  {
+    name: "Cecilia Kohler",
+    vehicle: "Smart V90",
+    id: "33",
+  },
+  {
+    name: "Elijah Muller I",
+    vehicle: "Honda Cruze",
+    id: "34",
+  },
+  {
+    name: "Mrs. Omar Koss",
+    vehicle: "Ford Model X",
+    id: "35",
+  },
+  {
+    name: "Catherine Ullrich",
+    vehicle: "Lamborghini Golf",
+    id: "36",
+  },
+  {
+    name: "Patricia Hodkiewicz",
+    vehicle: "Polestar Land Cruiser",
+    id: "37",
+  },
+  {
+    name: "Marian Boehm",
+    vehicle: "Smart Model 3",
+    id: "38",
+  },
+  {
+    name: "Tasha Dooley",
+    vehicle: "Ferrari XC90",
+    id: "40",
+  },
+  {
+    name: "Vanessa Schumm",
+    vehicle: "Ford Fortwo",
+    id: "41",
+  },
+];
+
+export default owners;
